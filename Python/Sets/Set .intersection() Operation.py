de313095fee16e741b7c55921897fe995cc0ece0
vrushali <50298146+vrushalipr@users.noreply.github.com>
2022-11-12 14:55:06 -0500

    Set .intersection() Operation

---
+++ Python/Sets/Set .intersection() Operation.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+nl = list(input().split())
+b = int(input())
+bl = list(input().split())
+
+s1 = set(nl)
+s2 = set(bl)
+
+print(len(s1.intersection(s2)))
